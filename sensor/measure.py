98c55850ffbb91b61ac3530235fe9b723900706c
wei <wei.huang@tiendeo.com>
2018-10-20 16:44:44 +0200

    message-wei-velo_joystick

--- sensor/measure.py
+++ sensor/measure.py
@@ -56,18 +56,27 @@ while 1:
 
     red = (255, 0, 0)
 
-    while True:
-        acceleration = sense.get_accelerometer_raw()
-        x = acceleration['x']
-        y = acceleration['y']
-        z = acceleration['z']
+    
+    acceleration = sense.get_accelerometer_raw()
+    x = acceleration['x']
+    y = acceleration['y']
+    z = acceleration['z']
 
-        x = int(x)
-        y = int(y)
-        z = int(z)
-        sense.show_message("Tu velcidad: x={x}, y={y}, z={z}".format(x=x,y=y,z=z) )
-        if x > 0.01 or y > 0.01 or z > 0.01:
-            sense.show_letter("!", red)
-        else:
-            sense.clear()
-    time.sleep(5)
+    x = round(x,2)
+    y = round(y,2)
+    z = round(z,2)
+    sense.show_message("Tu velcidad: x={x}, y={y}, z={z}".format(x=x,y=y,z=z) )
+    if x > 0.01 or y > 0.01 or z > 0.01:
+        sense.show_letter("!", red,scroll_speed=1)
+    else:
+        sense.clear()
+    time.sleep(2)
+    # Tell the program which function to associate with which direction
+    sense.stick.direction_up = sense.clear(255, 0, 0)
+    sense.stick.direction_down = sense.clear(0, 0, 255)
+    sense.stick.direction_left = sense.clear(0, 255, 0)
+    sense.stick.direction_right = sense.clear(255, 255, 0)
+    sense.stick.direction_middle = sense.clear    # Press the enter key
+
+    while True:
+        pass  # This keeps the program running to receive joystick events
