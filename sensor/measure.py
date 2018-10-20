ca0799a8fc10af604832931902665d1ce4ab0596
wei <wei.huang@tiendeo.com>
2018-10-20 17:00:11 +0200

    joystick

--- sensor/measure.py
+++ sensor/measure.py
@@ -8,73 +8,73 @@ influx_client.create_database('balena-sense')
 
 sense = SenseHat()
 
-sense.clear()
-sense.load_image("balena.png")
-time.sleep(2)
-sense.show_message("balena")
+# sense.clear()
+# sense.load_image("balena.png")
+# time.sleep(2)
+# sense.show_message("balena")
 
-blue = (0, 0, 255)
-yellow = (255, 255, 0)
-count = 0
+# blue = (0, 0, 255)
+# yellow = (255, 255, 0)
+# count = 0
 
-measurements = [
-    {
-        'measurement': 'temperature',
-        'fields': {
-            'value': float(sense.temperature)
-        }
-    }
-]
+# measurements = [
+#     {
+#         'measurement': 'temperature',
+#         'fields': {
+#             'value': float(sense.temperature)
+#         }
+#     }
+# ]
 
-measurements.extend([
-    {
-        'measurement': 'humidity',
-        'fields': {
-            'value': float(sense.humidity)
-        }
-    }
-])
+# measurements.extend([
+#     {
+#         'measurement': 'humidity',
+#         'fields': {
+#             'value': float(sense.humidity)
+#         }
+#     }
+# ])
 
-measurements.extend([
-    {
-        'measurement': 'pressure',
-        'fields': {
-            'value': float(sense.pressure)
-        }
-    }
-])
+# measurements.extend([
+#     {
+#         'measurement': 'pressure',
+#         'fields': {
+#             'value': float(sense.pressure)
+#         }
+#     }
+# ])
 
-sense.set_pixel(0, count, 0, 255, 0)
-count = count+1
-if count == 8:
-    count = 0
-sense.set_pixel(0, count, 0, 0, 0)
-influx_client.write_points(measurements)
-sense.show_message("Hello Wei", text_colour=yellow, back_colour=blue)
-sense.show_message("Temperatue: {}".format(int(sense.temperature)))
+# sense.set_pixel(0, count, 0, 255, 0)
+# count = count+1
+# if count == 8:
+#     count = 0
+# sense.set_pixel(0, count, 0, 0, 0)
+# influx_client.write_points(measurements)
+# sense.show_message("Hello Wei", text_colour=yellow, back_colour=blue)
+# sense.show_message("Temperatue: {}".format(int(sense.temperature)))
 
-red = (255, 0, 0)
+# red = (255, 0, 0)
 
 
-acceleration = sense.get_accelerometer_raw()
-x = acceleration['x']
-y = acceleration['y']
-z = acceleration['z']
+# acceleration = sense.get_accelerometer_raw()
+# x = acceleration['x']
+# y = acceleration['y']
+# z = acceleration['z']
 
-x = round(x,2)
-y = round(y,2)
-z = round(z,2)
-sense.show_message("Tu velcidad: x={x}, y={y}, z={z}".format(x=x,y=y,z=z) )
-if x > 0.01 or y > 0.01 or z > 0.01:
-    sense.show_letter("!", red,scroll_speed=1)
-else:
-    sense.clear()
-time.sleep(2)
+# x = round(x,2)
+# y = round(y,2)
+# z = round(z,2)
+# sense.show_message("Tu velcidad: x={x}, y={y}, z={z}".format(x=x,y=y,z=z) )
+# if x > 0.01 or y > 0.01 or z > 0.01:
+#     sense.show_letter("!", red,scroll_speed=1)
+# else:
+#     sense.clear()
+# time.sleep(2)
 
 e = (0, 0, 0)
 w = (255, 255, 255)
 sense.clear()
-while 1:
+while True:
     for event in sense.stick.get_events():
         # Check if the joystick was pressed
         if event.action == "pressed":
