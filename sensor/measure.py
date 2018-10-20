283564e7cfac41d693422940267e704a59f207bc
wei <wei.huang@tiendeo.com>
2018-10-20 18:29:08 +0200

    joystick

--- sensor/measure.py
+++ sensor/measure.py
@@ -10,7 +10,7 @@ influx_client.create_database('balena-sense')
 sense = SenseHat()
 
 sense.clear()
-sense.load_image("balena.png")
+sense.load_image("heart.png")
 
 blue = (0, 0, 255)
 yellow = (255, 255, 0)
