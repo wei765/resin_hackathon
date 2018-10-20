837f5814b74457f3a4a0d092a98ebc93b0d3b0a9
wei <wei.huang@tiendeo.com>
2018-10-20 16:51:43 +0200

    message-wei-velo_joystick

--- sensor/measure.py
+++ sensor/measure.py
@@ -56,25 +56,42 @@ while 1:
 
     red = (255, 0, 0)
 
+    
+    acceleration = sense.get_accelerometer_raw()
+    x = acceleration['x']
+    y = acceleration['y']
+    z = acceleration['z']
+
+    x = round(x,2)
+    y = round(y,2)
+    z = round(z,2)
+    sense.show_message("Tu velcidad: x={x}, y={y}, z={z}".format(x=x,y=y,z=z) )
+    if x > 0.01 or y > 0.01 or z > 0.01:
+        sense.show_letter("!", red,scroll_speed=1)
+    else:
+        sense.clear()
+    time.sleep(2)
+
+    e = (0, 0, 0)
+    w = (255, 255, 255)
+    sense.clear()
     while True:
-        acceleration = sense.get_accelerometer_raw()
-        x = acceleration['x']
-        y = acceleration['y']
-        z = acceleration['z']
-
-        x = round(x,2)
-        y = round(y,2)
-        z = round(z,2)
-        sense.show_message("Tu velcidad: x={x}, y={y}, z={z}".format(x=x,y=y,z=z) )
-        if x > 0.01 or y > 0.01 or z > 0.01:
-            sense.show_letter("!", red,scroll_speed=1)
-        else:
-            sense.clear()
-        time.sleep(2)
-        # Tell the program which function to associate with which direction
-        sense.stick.direction_up = sense.clear(255, 0, 0)
-        sense.stick.direction_down = sense.clear(0, 0, 255)
-        sense.stick.direction_left = sense.clear(0, 255, 0)
-        sense.stick.direction_right = sense.clear(255, 255, 0)
-        sense.stick.direction_middle = sense.clear    # Press the enter key
-
+        for event in sense.stick.get_events():
+            # Check if the joystick was pressed
+            if event.action == "pressed":
+            
+                # Check which direction
+                if event.direction == "up":
+                    sense.show_letter("U")      # Up arrow
+                elif event.direction == "down":
+                    sense.show_letter("D")      # Down arrow
+                elif event.direction == "left": 
+                    sense.show_letter("L")      # Left arrow
+                elif event.direction == "right":
+                    sense.show_letter("R")      # Right arrow
+                elif event.direction == "middle":
+                    sense.show_letter("M")      # Enter key
+            
+                # Wait a while and then clear the screen
+                time.sleep(0.5)
+                sense.clear()
